3942ceab2a13a2156c928dabdf3f2d9a57afc8cc
Chad DeRosier <chad.derosier@tutanota.com>
2023-11-11 22:36:11 -0800

    More stuff

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+//! PCNT decoding a rotary encoder
+//!
+//! To try this out, connect a rotary encoder to pins 5 and 6, the common should be grounded
+//!
+//! Note that PCNT only track a singed 16bit value.  We use interrupts to detect a LOW and HIGH
+//! threshold and track how much that accounts for and provide an i64 value result
+
 // sudo adduser <user> dialout
 // sudo chmod a+rw /dev/ttyACM0
 //
@@ -12,10 +19,10 @@ use embedded_svc::{
     http::Method,
     io::Write,
 };
-use esp_idf_hal::delay::Delay;
-use esp_idf_hal::{gpio::*, peripherals::Peripherals};
 use esp_idf_hal::adc::config::Config as adc_Config;
 use esp_idf_hal::adc::*;
+use esp_idf_hal::delay::Delay;
+use esp_idf_hal::{gpio::*, peripherals::Peripherals};
 // use esp_idf_hal::adc::attenuation;
 //use esp_idf_hal::adc::{AdcChannelDriver, AdcDriver, Attenuation};
 use esp_idf_hal::ledc::{config::TimerConfig, LedcDriver, LedcTimerDriver};
@@ -35,19 +42,24 @@ use std::{
     sync::{Arc, Mutex},
     thread,
     thread::sleep,
-    time::Duration,
+    time::{Duration, Instant},
 };
 
 use device::{Action, Device};
 use encoder::Encoder;
 
+#[cfg(all(not(feature = "riscv-ulp-hal"), any(esp32, esp32s2, esp32s3)))]
 fn main() {
-    esp_idf_sys::link_patches(); //Needed for esp32-rs
-    println!("Entered Main function!");
-    let peripherals = Peripherals::take().unwrap();
-    let sys_loop = EspSystemEventLoop::take().unwrap();
-    let nvs = EspDefaultNvsPartition::take().unwrap();
+    //    use anyhow::Context;
 
+    // Temporary. Will disappear once ESP-IDF 4.4 is released, but for now it is necessary to call this function once,
+    // or else some patches to the runtime implemented by esp-idf-sys might not link properly.
+    esp_idf_sys::link_patches();
+
+    println!("setup pins");
+    let peripherals = Peripherals::take().unwrap();
+    let _sys_loop = EspSystemEventLoop::take().unwrap();
+    let _nvs = EspDefaultNvsPartition::take().unwrap();
 
     let light_1 = Arc::new(Mutex::new(Device {
         name: "bed light".to_string(),
@@ -60,143 +72,72 @@ fn main() {
             Action::Set,
         ]),
         default_target: 3,
-        duty_cycles: [0, 6, 12, 25, 50, 96],
+        duty_cycles: [0, 2, 4, 8, 16, 32, 64, 96],
         target: 0,
-        freq_kHz: 1,
-//        freq_Hz: 100.Hz().into(),
+        freq_Hz: 1000,
     }));
 
-    // let freq_khz = { (1. /light_1.lock().unwrap().period_ms as f64 * 1000.).round() as usize };
-    let driver_1 = Arc::new(Mutex::new(LedcDriver::new(
-        peripherals.ledc.channel0,
-        LedcTimerDriver::new(
-            peripherals.ledc.timer0,
-            &TimerConfig::new().frequency(100.Hz().into()),
-        ).unwrap(),
-        peripherals.pins.gpio7,
-    ).unwrap()));
-
-    // roof_vent Manager
+    let light_1_clone = light_1.clone();
     thread::spawn(move || {
-        let voltages = [680, 1360, 2040, 2720, 3000, 50000];
-        let mut pin_a = peripherals.pins.gpio5;
-        let mut pin_b = peripherals.pins.gpio6;
-        let encoder = Encoder::new(peripherals.pcnt0, &mut pin_a, &mut pin_b).unwrap();
-        let mut last_value = 0;
+        println!("setup encoder");
+        let mut pin_a_1 = peripherals.pins.gpio5;
+        let mut pin_b_1 = peripherals.pins.gpio6;
+        let encoder_1 = Encoder::new(peripherals.pcnt0, &mut pin_a_1, &mut pin_b_1).unwrap();
+
+        let mut last_encoder_value = 0i64;
+        let mut last_encoder_time = Instant::now();
         loop {
-            {
-                let value = encoder.get_value().unwrap();
-                if value < last_value {
-                    println!("-- {value}");
-                    last_value = value;
-                } else if value > last_value {
-                    println!("+ {value}");
-                    last_value = value;
+            let encoder_value = encoder_1.get_value().unwrap();
+            if encoder_value != last_encoder_value {
+                let current_time = Instant::now();
+                let time_since_last_check = current_time.duration_since(last_encoder_time);
+                if time_since_last_check > Duration::from_millis(100) {
+                    {
+                        let mut light = light_1_clone.lock().unwrap();
+                        if encoder_value > last_encoder_value {
+                            let _ = light.take_action(Action::Up, None);
+                        } else {
+                            let _ = light.take_action(Action::Down, None);
+                        }
+                    }
+                    last_encoder_time = Instant::now();
                 }
+                last_encoder_value = encoder_value;
             }
-            Delay::delay_ms(200);
+            Delay::delay_ms(100u32);
         }
     });
 
-    loop {
-        sleep(Duration::new(1000, 0));
-    }
+    let light_1_clone = light_1.clone();
+    thread::spawn(move || {
+        let mut driver_1 = LedcDriver::new(
+            peripherals.ledc.channel0,
+            LedcTimerDriver::new(
+                peripherals.ledc.timer0,
+                &TimerConfig::new().frequency({ light_1.lock().unwrap() }.freq_Hz.Hz()),
+            )
+            .unwrap(),
+            peripherals.pins.gpio7,
+        ).unwrap();
+        let max_duty = driver_1.get_max_duty();
+        loop {
+            {
+                let light_1 = light_1_clone.lock().unwrap();
+
+                let duty_cycle = light_1.get_duty_cycle() * max_duty / 100;
+                let _ = driver_1.set_duty(duty_cycle);
+                println!("Duty cycle: {duty_cycle}");
+            }
+            Delay::delay_ms(100u32);
+        }
+    });
 }
 
-#[cfg(all(not(feature = "riscv-ulp-hal"), any(esp32, esp32s2, esp32s3)))]
-// esp-idf encoder implementation using v4 pcnt api
-mod encoder {
-    use std::cmp::min;
-    use std::sync::atomic::AtomicI64;
-    use std::sync::atomic::Ordering;
-    use std::sync::Arc;
-
-    use esp_idf_hal::gpio::AnyInputPin;
-    use esp_idf_hal::gpio::InputPin;
-    use esp_idf_hal::pcnt::*;
-    use esp_idf_hal::peripheral::Peripheral;
-    use esp_idf_sys::EspError;
-
-    const LOW_LIMIT: i16 = -100;
-    const HIGH_LIMIT: i16 = 100;
-
-    pub struct Encoder<'d> {
-        unit: PcntDriver<'d>,
-        approx_value: Arc<AtomicI64>,
-    }
-
-    impl<'d> Encoder<'d> {
-        pub fn new<PCNT: Pcnt>(
-            pcnt: impl Peripheral<P = PCNT> + 'd,
-            pin_a: impl Peripheral<P = impl InputPin> + 'd,
-            pin_b: impl Peripheral<P = impl InputPin> + 'd,
-        ) -> Result<Self, EspError> {
-            let mut unit = PcntDriver::new(
-                pcnt,
-                Some(pin_a),
-                Some(pin_b),
-                Option::<AnyInputPin>::None,
-                Option::<AnyInputPin>::None,
-            )?;
-            unit.channel_config(
-                PcntChannel::Channel0,
-                PinIndex::Pin0,
-                PinIndex::Pin1,
-                &PcntChannelConfig {
-                    lctrl_mode: PcntControlMode::Reverse,
-                    hctrl_mode: PcntControlMode::Keep,
-                    pos_mode: PcntCountMode::Decrement,
-                    neg_mode: PcntCountMode::Increment,
-                    counter_h_lim: HIGH_LIMIT,
-                    counter_l_lim: LOW_LIMIT,
-                },
-            )?;
-            unit.channel_config(
-                PcntChannel::Channel1,
-                PinIndex::Pin1,
-                PinIndex::Pin0,
-                &PcntChannelConfig {
-                    lctrl_mode: PcntControlMode::Reverse,
-                    hctrl_mode: PcntControlMode::Keep,
-                    pos_mode: PcntCountMode::Increment,
-                    neg_mode: PcntCountMode::Decrement,
-                    counter_h_lim: HIGH_LIMIT,
-                    counter_l_lim: LOW_LIMIT,
-                },
-            )?;
-
-            unit.set_filter_value(min(10 * 80, 1023))?;
-            unit.filter_enable()?;
-
-            let approx_value = Arc::new(AtomicI64::new(0));
-            // unsafe interrupt code to catch the upper and lower limits from the encoder
-            // and track the overflow in `value: Arc<AtomicI64>` - I plan to use this for
-            // a wheeled robot's odomerty
-            unsafe {
-                let approx_value = approx_value.clone();
-                unit.subscribe(move |status| {
-                    let status = PcntEventType::from_repr_truncated(status);
-                    if status.contains(PcntEvent::HighLimit) {
-                        approx_value.fetch_add(HIGH_LIMIT as i64, Ordering::SeqCst);
-                    }
-                    if status.contains(PcntEvent::LowLimit) {
-                        approx_value.fetch_add(LOW_LIMIT as i64, Ordering::SeqCst);
-                    }
-                })?;
-            }
-            unit.event_enable(PcntEvent::HighLimit)?;
-            unit.event_enable(PcntEvent::LowLimit)?;
-            unit.counter_pause()?;
-            unit.counter_clear()?;
-            unit.counter_resume()?;
-
-            Ok(Self { unit, approx_value })
-        }
-
-        pub fn get_value(&self) -> Result<i64, EspError> {
-            let value =
-                self.approx_value.load(Ordering::Relaxed) + self.unit.get_counter_value()? as i64;
-            Ok(value)
-        }
+#[cfg(not(all(not(feature = "riscv-ulp-hal"), any(esp32, esp32s2, esp32s3))))]
+fn main() {
+    use esp_idf_hal::delay::FreeRtos;
+    println!("pcnt peripheral not supported on this device!");
+    loop {
+        FreeRtos::delay_ms(100u32);
     }
 }
